75e42e43123dc47edcef80f57d0f3c8f32eaf7b9
Phan Huy Hoang <phanhoang1366@gmail.com>
2025-03-06 10:39:12 +0700

    Try to answer the questions

---
+++ Lab01/Code/JavaBasics/src/JavaBasics/ChoosingOption.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+	public static void main(String[] args) {
+		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
+		
+		if(option == JOptionPane.CANCEL_OPTION) System.exit(1);
+
+		JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
+		
+		Object[] options = {"I do", "I don't"};
+		
+		option = JOptionPane.showOptionDialog(null, "Do you want to change to the first class ticket?", "Choose an option", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		
+		JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
+		
+		System.exit(0);
+	}
+}
--- Lab01/Code/JavaBasics/src/module-info.java
+++ Lab01/Code/JavaBasics/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module JavaBasics {
+	requires java.desktop;
 }
