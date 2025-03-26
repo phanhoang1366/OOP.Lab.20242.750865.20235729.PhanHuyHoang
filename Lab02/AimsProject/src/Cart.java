d3dd828a1dd213dfd98bbf7996650ef37f349959
Phan Huy Hoang <phanhoang1366@gmail.com>
2025-03-26 23:22:47 +0700

    Initial commit for AimsProject

---
+++ Lab02/AimsProject/src/Cart.java
@@ -0,0 +1,18 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+
+    }
+
+    public float totalCost() {
+        return 0;
+    }
+    
+}
