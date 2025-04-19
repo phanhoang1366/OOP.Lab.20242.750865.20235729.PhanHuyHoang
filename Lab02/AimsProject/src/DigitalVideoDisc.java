3d41f3fe7a7c610901461b2ca59ea9bf67d923d5
Phan Huy Hoang <phanhoang1366@gmail.com>
2025-04-19 10:36:34 +0700

    hotfix passing parameter

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -10,20 +10,40 @@ public class DigitalVideoDisc {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getCategory() {
         return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public String getDirector() {
         return director;
     }
 
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
     public int getLength() {
         return length;
     }
 
+    public void setLength(int length) {
+        this.length = length;
+    }
+
     public float getCost() {
         return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
     }
 
     // public DigitalVideoDisc() {
